1917febf2ee153b1474d4dc7b7a4c82112660d7a
luke01455 <luke0145555@hotmail.co.uk>
2019-08-05 12:49:50 +0100

    how modules work passing - passing objects and variables from one js file to another

--- src/js/index.js
+++ src/js/index.js
@@ -1,6 +1,7 @@
-// Global app controller
-import num from './test';
+import str from './models/Search';
 
+//import { add as a, multiply as m, ID } from './views/searchView'
 
-const x = 23;
-console.log(`i imported ${num} from another module test js Variable x is ${x}`);
+import * as searchView from './views/searchView'
+
+console.log(`using imported functions ! ${searchView.add(searchView.ID, 2)} and ${searchView.multiply(3, 5)} and ${str}`)
