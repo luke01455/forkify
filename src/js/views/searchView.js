75a92c953671154cc7007e154a98bcbcde370ba0
luke01455 <luke0145555@hotmail.co.uk>
2019-08-05 20:10:37 +0100

    finishing the search view to limit the amount of title text on the searched items titles.

--- src/js/views/searchView.js
+++ src/js/views/searchView.js
@@ -8,7 +8,24 @@ export const clearInput = () => {
 export const clearResults = () => {
     elements.searchResList.innerHTML = '';
 };
+
+const limitRecipeTitle = (title, limit = 17) => {
+    const newTitle = [];
+    if(title.length >= limit) {
+        title.split(' ').reduce((acc, cur) => {
+            if(acc + cur.length <= limit) {
+                newTitle.push(cur)
+            }
+            return acc + cur.length;
+        }, 0)
+        return `${newTitle.join(' ')} ...`
+    }
+    return title;
+}
+
+
 const renderRecipe = reciperend => {
+    const newtitle = limitRecipeTitle(reciperend.recipe.label)
     const markup = `
     <li>
         <a class="results__link" href="#${reciperend.recipe.calories}">
@@ -16,7 +33,7 @@ const renderRecipe = reciperend => {
                 <img src=${reciperend.recipe.image} alt="${reciperend.recipe.label}">
             </figure>
             <div class="results__data">
-                <h4 class="results__name">${reciperend.recipe.label}</h4>
+                <h4 class="results__name">${newtitle}</h4>
                 <p class="results__author">${reciperend.recipe.source}</p>
             </div>
         </a>
@@ -26,6 +43,6 @@ const renderRecipe = reciperend => {
     
 };
 export const renderResults = recipes => {
-    console.log(recipes)
+    console.log(recipes);
     recipes.forEach(renderRecipe);
 };
